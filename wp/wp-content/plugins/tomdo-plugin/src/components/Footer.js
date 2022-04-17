2009a2917f3f79d95a47ba587dc1d58e694fa342
TomDo <tom.do@bjmdigital.com.au>
2022-04-17 12:11:18 +1000

    finished first half of footer

--- wp/wp-content/plugins/tomdo-plugin/src/components/Footer.js
+++ wp/wp-content/plugins/tomdo-plugin/src/components/Footer.js
@@ -13,10 +13,47 @@ export default function Footer() {
                     <div>VinylMaster XPT</div>
                     <div>View Price List</div>
                 </div>
-                <div class = "col">5</div>
-                <div class = "col">4</div>
-                <div class = "col">3</div>
-                <div class = "col">2</div>
+                <div class = "col">
+                    <div className="headcol">SHOP BY INDUSTRY</div>
+                    <div>Craft &amp; Hobby</div>
+                    <div>Box, Card &amp; Paper</div>
+                    <div>T-Shirts and Caps</div>
+                    <div>Labels, Decals &amp; Stickers</div>
+                    <div>Vinyl Lettering</div>
+                    <div>Print Shops</div>
+                    <div>Sign Shops</div>
+                    <div>Production</div>
+                    <div>Government</div>
+                    <div>Education</div>
+                </div>
+                <div class = "col">
+                    <div className="headcol">SUPPORT</div>
+                    <div>Help &amp; Support Centre</div>
+                    <div>Lodge a Ticket</div>
+                </div>
+                <div class = "col">
+                    <div className="headcol">FOR EDUCATION</div>
+                    <div>Discount for Student and <br/> Teachers</div><br/>
+                    <div>Schools and Universities</div>
+                    <div>Free Offer for the Art of Graphic <br/> Design <br/> and Related Subjects</div><br/><br/>
+                    <br/>
+                    <div className="headcol">FOR BUSINESS</div>
+                    <div>VinylMaster for Dealers &amp; <br/> Resellers</div><br/>
+                    <div>OEM &amp; CustomWare</div>
+                    <div>Corporate Inquiries</div>
+                </div>
+                <div class = "col">
+                    <div className="headcol">VINYLMASTER ACCOUNT</div>
+                    <div>Login to Customer Portal</div>
+                    <div>Update Software</div>
+                    <br/>
+                    <div className="headcol">VINYLMASTER</div>
+                    <div>Download &amp; Install</div>
+                    <div>Contact Us</div>
+                    <div>About Us</div>
+                    <div>Dealers &amp; Resellers</div>
+                    <div>Supported Cutters</div>
+                </div>
             </div>  
         </header>
     );
