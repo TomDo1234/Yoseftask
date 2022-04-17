dba637a0dd5de8a11ea0593bc1bd1104320f2fad
TomDo <tom.do@bjmdigital.com.au>
2022-04-17 20:57:16 +1000

    added non static section summary blocks

--- wp/wp-content/plugins/tomdo-plugin/src/index.js
+++ wp/wp-content/plugins/tomdo-plugin/src/index.js
@@ -4,7 +4,8 @@ import Footer from './components/Footer';
 import LoveUsing from './components/LoveUsing';
 import Static1 from './components/Static1';
 import SystemReqs from './components/SystemReqs';
-import Goodideas from './components/Goodideas';
+import {RichText} from '@wordpress/block-editor';
+import './components/Sectionsummary.css';
 
 registerBlockType('gtcb-blocks/custom-block',{
     title:'VinylMaster Footer',
@@ -52,10 +53,53 @@ registerBlockType('gtcb-blocks/custom-block5',{
 });
 
 registerBlockType('gtcb-blocks/custom-block6',{
-    title:'VinylMaster GoodIdeas',
+    title:'VinylMaster Section Summary',
     icon: 'index-card',
     category : 'common',
 
-    edit: () => (<Goodideas/>),
-    save: () => (<Goodideas/>)
+    edit: (props) => {
+        let {attributes: {headerText,bodyText,customcolor}, setAttributes } = props;
+
+        return(
+            <div className= 'sectionsummary'> 
+                <RichText
+                tagName="h3"
+                placeholder={'Header Text'}
+                value = {headerText}
+                onChange={(value) => setAttributes({headerText: value})}                
+                /> 
+                <RichText
+                tagName="div"
+                placeholder={'Body Text'}
+                value = {bodyText}
+                onChange={(value) => setAttributes({bodyText: value})}      
+                className="thetext"
+                />
+                <RichText
+                tagName="div"
+                placeholder={'Custom Color'}
+                value = {customcolor}
+                onChange={(value) => setAttributes({customcolor: value})}      
+                className="thetext"
+                />              
+            </div>                 
+        );
+    },
+    save: (props) => {
+        let {attributes: {headerText,bodyText,customcolor} } = props;
+
+        return(
+            <div className= {'sectionsummary '} style={{backgroundColor: customcolor}}> 
+                <RichText.Content
+                tagName="h3"
+                value = {headerText}            
+                /> 
+                <RichText.Content
+                tagName="div"
+                value = {bodyText}    
+                className="thetext"                
+                />             
+            </div>                 
+        );
+    }
 });
