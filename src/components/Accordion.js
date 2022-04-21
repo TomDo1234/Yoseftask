2dbd0ecea20e58c6a5af36310054fdb075d14359
TomDo <tom.do@bjmdigital.com.au>
2022-04-21 13:48:15 +1000

    progress with accordion, dynamic icons now 2

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -27,11 +27,10 @@ export default function Accordion({data,accordiondata}) {
 
                                                 let feature = iconForFeature.find((elem) => elem.feature.name === child.name);
                                                 let url = feature.icon.sourceUrl;
-                                                console.log(feature);
 
                                                 return (<li>
                                                     <div className='imageholder'>
-                                                        <img width="80" height="80" src='http://localhost:8000/wp-content/uploads/2022/04/Create-Contour.png' alt=""/>
+                                                        <img width="80" height="80" src={url} alt=""/>
                                                     </div>
                                                     <h5>{child.name}</h5>                                     
                                                 </li>);
